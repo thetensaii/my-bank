44813b0df300145f7528c84f464a3c93718f9c5e
Salimou <f.salimou@hotmail.fr>
2021-06-12 10:50:43 +0200

    Account Service

--- back/src/services/account.service.ts
+++ back/src/services/account.service.ts
@@ -2,8 +2,8 @@ import { Service } from "typedi";
 import { Factory } from "../models/factory";
 import bcrypt from "bcrypt"
 import config from "../config"
-import { AccountEntity } from "../entities/account.entity";
-import { UserPublicJSON } from "../entities/user.entity";
+import { AccountEntity, AccountJSON } from "../entities/account.entity";
+import { UserEntity, UserPublicJSON } from "../entities/user.entity";
 
 @Service({transient : true})
 export class AccountService {
@@ -21,6 +21,45 @@ export class AccountService {
         await this.factory.release();
 
         return accountEntity;
+    }
+
+    async findByUserID(id:number):Promise<AccountEntity[]>{
+        let accounts:AccountEntity[] = await this.factory.AccountModel.findByUserID(id);
+        await this.factory.release();
+
+        return accounts;
+    }
+
+    async create(user:UserPublicJSON, account:AccountJSON): Promise<AccountEntity>{
+        if(!user.id){ 
+            this.factory.release();
+            throw new Error("User doesn't exist");
+        }
+        
+        let userEntity:UserEntity|null = await this.factory.UserModel.findByID(user.id);
+        if(!userEntity){
+            this.factory.release();
+            throw new Error("User doesn't exist");
+        }
+        let accountEntity:AccountEntity|null = new AccountEntity(account);
+
+        try {
+            this.factory.beginTransaction();
+            let accountID:number = await this.factory.AccountModel.add(accountEntity);
+            accountEntity = await this.factory.AccountModel.findByID(accountID);
+            
+            if(!accountEntity) throw new Error(); 
+
+            this.factory.commit();
+            await this.factory.release()
+
+            return accountEntity;
+        } catch (error) {
+            await this.factory.rollback()
+            await this.factory.release()
+
+            throw new Error("Error while creating account")
+        }
     }
 
     async changeName(user:UserPublicJSON, accountID:number, name:string): Promise<AccountEntity>{
---
+++ back/tests/services/account.service.spec.ts
@@ -0,0 +1,177 @@
+import 'reflect-metadata';
+import { expect } from "chai";
+import Container from "typedi";
+import faker from "faker/locale/fr"
+
+import MySQLDatabase from '../../src/core/mysql.database';
+import mysql from "promise-mysql";
+
+import { UserJSON, UserEntity, UserPublicJSON } from "../../src/entities/user.entity";
+import { AccountEntity, AccountJSON } from '../../src/entities/account.entity';
+
+import { AccountService } from "../../src/services/account.service"
+import { AuthService } from "../../src/services/auth.service"
+import { UserService } from '../../src/services/user.service';
+
+
+describe("AccountService test", () => {
+    let accountService:AccountService;
+    let account:AccountJSON;
+    let accounts:AccountJSON[] = [];
+    let accountEntity:AccountEntity|null;
+
+
+    let user:UserJSON;
+    let userEntity:UserEntity;
+
+    let authService:AuthService;
+
+    before(async () => {
+        let l:number = Math.ceil(Math.random() * 15); // Number of accounts
+        
+        // Creating user
+        user = {
+            login: faker.internet.userName(),
+            firstname : faker.name.firstName(),
+            lastname: faker.name.lastName(),
+            email: faker.internet.email(),
+            password: faker.internet.password(),
+            is_admin: Boolean(Math.round(Math.random()))
+        };
+
+        authService = Container.get(AuthService);
+        userEntity = await authService.signUp(user);
+
+        for(let i = 0; i < l;++i){
+            accounts.push({
+                user_id : userEntity.id!,
+                name : faker.vehicle.vehicle(),
+                balance : faker.datatype.number()
+            })
+        }
+
+    });
+
+    after(async () => {    
+        let db:MySQLDatabase = Container.get(MySQLDatabase);
+        let conn:mysql.PoolConnection = await db.getConnection();
+
+        await conn.query("DELETE FROM users");
+        await conn.release();
+    })
+
+    beforeEach(async () => {    
+        accountService = Container.get(AccountService);
+    })
+
+    afterEach(async () => {    
+        let db:MySQLDatabase = Container.get(MySQLDatabase);
+        let conn:mysql.PoolConnection = await db.getConnection();
+
+        await conn.query("DELETE FROM accounts");
+        await conn.release();
+    })
+
+    it("should find null", async () => {
+        // ARRANGE
+
+        // ACT
+        accountEntity = await accountService.findByID(faker.datatype.number())
+
+        // ASSERT
+        expect(accountEntity).to.be.null;
+    });
+
+    it("should create Account", async () => {
+        // ARRANGE
+        let i:number = Math.floor(Math.random() * accounts.length);
+        account = accounts[i];
+        
+        // ACT
+        accountEntity = await accountService.create(userEntity.toPublicJSON(), account)
+
+        // ASSERT
+        expect(accountEntity).not.to.be.null
+        expect(accountEntity?.id).to.be.a("number")
+        expect(accountEntity?.name).to.be.equal(account.name);
+        expect(accountEntity?.balance).to.be.equal(account.balance);
+        expect(accountEntity?.created_at).to.be.a("Date");
+        expect(accountEntity?.updated_at).to.be.null;
+    });
+
+    it("should find Account info", async () => {
+        // ARRANGE
+        let i:number = Math.floor(Math.random() * accounts.length);
+        account = accounts[i];
+        accountEntity = await accountService.create(userEntity.toPublicJSON(), account)
+        
+        let accountID:number = accountEntity.id!;
+        let createdAt:Date = accountEntity.created_at!;
+
+        // ACT
+        accountService = Container.get(AccountService);
+        accountEntity = await accountService.findByID(accountID)
+
+        // ASSERT
+        expect(accountEntity).not.to.be.null
+        expect(accountEntity?.id).to.be.equal(accountID);
+        expect(accountEntity?.name).to.be.equal(account.name);
+        expect(accountEntity?.balance).to.be.equal(account.balance);
+        expect(accountEntity?.created_at).to.be.a("Date");
+        expect(accountEntity?.updated_at).to.be.null;
+    });
+  
+    it("should change Account name", async () => {
+        // ARRANGE
+        let i:number = Math.floor(Math.random() * accounts.length);
+        account = accounts[i];
+        accountEntity = await accountService.create(userEntity.toPublicJSON(), account)
+        
+        // ACT
+        accountService = Container.get(AccountService);
+        let accountID = accountEntity.id!;
+        let newName:string = faker.vehicle.vehicle();
+        accountEntity = await accountService.changeName(userEntity.toPublicJSON(), accountID, newName);
+
+        // ASSERT
+        expect(accountEntity).not.to.be.null
+        expect(accountEntity?.id).to.be.a("number")
+        expect(accountEntity?.name).to.be.equal(newName);
+        expect(accountEntity?.balance).to.be.equal(account.balance);
+        expect(accountEntity?.created_at).to.be.a("Date");
+        expect(accountEntity?.updated_at).to.be.a("Date");
+    });
+
+    it("should get all accounts", async () => {
+        // ARRANGE
+        for(let a of accounts){
+            accountService = Container.get(AccountService);
+            await accountService.create(userEntity.toPublicJSON(), a);
+        }
+
+        accountService = Container.get(AccountService);
+        
+        // ACT
+        let results:AccountEntity[] = await accountService.findAll();
+
+        // ASSERT
+        expect(results.length).to.be.equal(accounts.length);
+    })
+
+    it("should get accounts by User", async () => {
+        // ARRANGE
+        for(let a of accounts){
+            accountService = Container.get(AccountService);
+            await accountService.create(userEntity.toPublicJSON(), a);
+        }
+
+        accountService = Container.get(AccountService);
+        
+        // ACT
+        let results:AccountEntity[] = await accountService.findByUserID(userEntity.id!);
+
+        // ASSERT
+        expect(results.length).to.be.equal(accounts.length);
+    })
+
+});
